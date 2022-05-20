adb24c1bbe2dae306498296e1f96f257d718ae42
Daniel Garay <garaydaniel6@gmail.com>
2022-05-20 06:48:50 -0500

    Agregar archivos de proyecto.

---
+++ StoreApi/Controllers/ProductApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using StoreApi.Models.ApiModels.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApi.Controllers
+{
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+
+        [Route("CreateProduct")]
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] Product product)
+        {
+            try
+            {
+                string sku = product.SKU;
+                return Ok();
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode(400, e.Message);
+            }
+        }
+    }
+}
