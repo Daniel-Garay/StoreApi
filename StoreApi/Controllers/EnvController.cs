847c39f895c0963d57c28314fc5a7e80f8a67be5
Daniel Garay <garaydaniel6@gmail.com>
2022-09-29 12:47:41 -0500

    test

---
+++ StoreApi/Controllers/EnvController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EnvController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public EnvController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_config.GetSection("Message").Value);
+        }
+    }
+
+
+}
